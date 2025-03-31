9e22598a017f6ead686e4ae5b9c43a25f3bc1b27
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-03-31 16:34:39 -0300

    Condição de aprovação

---
+++ EX5.py
@@ -0,0 +1,10 @@
+nota1=float(input("Digite a primeira nota: ").replace(',','.'))
+nota2=float(input("Digite a segunda nota: ").replace(',','.'))
+nota3=float(input("Digite a terceira nota: ").replace(',','.'))
+
+media = (nota1+nota2+nota3)/3
+
+if media>=7.0:
+    print(f"Parabéns, aluno aprovado. Nota:{media:.2f}")
+else:
+    print(f"Aluno reprovado. Nota:{media:.2f}")
