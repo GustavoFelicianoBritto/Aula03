77056888b08c0afb3601ef9ab4c02d8c54d30196
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-03-31 16:14:34 -0300

    Numeros em ordem crescente

---
+++ Ex4.py
@@ -0,0 +1,13 @@
+a= int(input("Digite o valor para A: "))
+b = int(input("Digite o valor para B: "))
+
+if a<b:
+    print(f"{a}\n{b}")
+elif(a==b):
+    print("Os números são iguais")
+else:
+    print(f"{b}\n{a}")
+
+
+
+
