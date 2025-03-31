cb2474fd8339c70f84752151af7718480d4a1789
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-03-31 15:59:12 -0300

    Atividade troca de valores

---
+++ EX3TrocaValores.py
@@ -0,0 +1,13 @@
+from time import process_time_ns
+
+A= int(input("Digite o valor para A: "))
+B = int(input("Digite o valor para B: "))
+
+print(f"Inicialmente o valor de A é {A}\n"
+      f"Inicialmente o valor de B é {B} ")
+aux = A
+A = B
+B= aux
+
+print(f"Agora o valor de A é {A}\n"
+      f"Agora o valor de B é {B} ")
