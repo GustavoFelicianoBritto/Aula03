bae9bf4774645d4a37cac408687e02b902743648
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-04-02 14:41:32 -0300

    Código base com aceitação de minusculo e maiusculo

--- EX7.py
+++ EX7.py
@@ -5,20 +5,22 @@ combustivelAtual= ""
 gasolina=5.80
 etanol = 4.90
 
-while tipoCombustivel != "e" and tipoCombustivel != "g":
+while tipoCombustivel != "e" and  tipoCombustivel != "E" and tipoCombustivel != "g" and tipoCombustivel != "G":
 
-    tipoCombustivel = input("Escolha o tipo do combustível\n E - Etanol, G - Gasolina: ").lower()
+    #print(tipoCombustivel)
+    tipoCombustivel = input("Escolha o tipo do combustível\n E - Etanol, G - Gasolina: ")
 
 
-    if tipoCombustivel == "e":
+    if tipoCombustivel == "e" or tipoCombustivel=="E":
         print("Etanol selecionado")
         total = litros* etanol
         combustivelAtual = "Etanol"
 
-    elif tipoCombustivel == "g":
+    elif tipoCombustivel == "g" or tipoCombustivel== "G":
         print("Gasolina selecionada")
         total = litros * gasolina
         combustivelAtual = "Gasolina"
+
     else:
         print("Opção inválida, insira novamente")
 
